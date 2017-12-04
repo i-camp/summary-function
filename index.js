bc15342762364423b573bc943cb1af00122cec75
yamamotoya <yamamotoya@istyle.co.jp>
2017-12-05 02:42:19 +0900

    Add function to get current game id

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-exports.hourly_job =
-  functions.pubsub.topic('hourly-tick').onPublish((event) => {
-      console.log("This job is ran every hour!")
-        });
+exports.summery_score =
+  functions.pubsub.topic('every-minute-tick').onPublish((event) => {
+    // Get current game id on e-party2017/currentGame/id
+    var db = admin.database();
+    var current_game_id_ref = db.ref("/currentGame/id");
+
+    current_game_id_ref.on("value", function(snapshot) { 
+      var current_game_id = snapshot.val();
+      console.log("current game id: " + snapshot.val());
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+
+ 
+    // Get target of the bullets for summary
+    /*
+    var current_game_commits_ref = db.ref("/Commits" + current_game_id)
+
+    current_game_commits_ref.on("value", function(snapshot) {
+      const current_game_commits = snapshot.val();
+      console.log("current_game_commits" + snapshot.val());
+    }, function (errorObject) {
+      console.log("The read failed" + errorObject.code);
+    });
+    */
+
+    // Calc for each target  
+
+    // Post liveHp on e-party2017/currentGame/targets/*/liveHp
+  });
