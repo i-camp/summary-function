af3cd0f2dce2630fdd2d8490fc437da0ac0b80a5
yamamotoya <yamamotoya@istyle.co.jp>
2017-12-05 04:52:33 +0900

    Get commits

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ exports.summery_score =
     var db = admin.database();
     var current_game_id_ref = db.ref("/currentGame/id");
 
+    /*
     current_game_id_ref.on("value", function(snapshot) { 
       var current_game_id = snapshot.val();
       console.log("current game id: " + snapshot.val());
     }, function (errorObject) {
       console.log("The read failed: " + errorObject.code);
     });
+    */
 
- 
+
     // Get target of the bullets for summary
     /*
     var current_game_commits_ref = db.ref("/Commits" + current_game_id)
@@ -32,4 +34,25 @@ exports.summery_score =
     // Calc for each target  
 
     // Post liveHp on e-party2017/currentGame/targets/*/liveHp
+
+    current_game_id_ref.on("value", function(snapshot) { 
+      var value = snapshot.val();
+      console.log("current: " + value);
+
+      var current_game_commits_ref = db.ref("/commits/" + value);
+      current_game_commits_ref.on("value", function(snapshot){
+        const commits = snapshot.val();
+        console.log(commits);
+	var key;
+	var plus_sum = 0;
+	var minus_sum = 0;
+
+	for (key in commits){
+	  plus_sum += commits[key].plus;
+	  minus_sum += commits[key].minus;
+	}
+	console.log(plus_sum);
+
+      });
+    });
   });
